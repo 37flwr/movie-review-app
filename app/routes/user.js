b312d9631faddaad6938d3a3ec76e9664af894f0
37flwr <nesterishinyarik@gmail.com>
2023-06-23 20:53:15 +0300

    + user validation

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { create } = require("../controllers/user");
+const { validate, userValidator } = require("../middlewares/validator");
 
 const router = express.Router();
 
-router.post("/create", create);
+router.post("/create", userValidator, validate, create);
 
 module.exports = router;
