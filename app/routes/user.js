e1b4e83eb17dca543388cce432a884a60e5594a6
37flwr <nesterishinyarik@gmail.com>
2023-06-24 16:58:28 +0300

    + reset password route

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,6 +3,7 @@ const {
   create,
   verifyEmail,
   resendEmailVerificationToken,
+  resetPassword,
 } = require("../controllers/user");
 const { validate, userValidator } = require("../middlewares/validator");
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", userValidator, validate, create);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-email-verification-token", resendEmailVerificationToken);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
