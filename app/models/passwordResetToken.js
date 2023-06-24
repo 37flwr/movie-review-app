1b48c70df52268cd683a4e6ee3f49c31c5b50a28
37flwr <nesterishinyarik@gmail.com>
2023-06-24 16:33:25 +0300

    + password reset model

---
+++ app/models/passwordResetToken.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const passwordResetTokenSchema = mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  token: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    expires: 3600,
+    default: Date.now(),
+  },
+});
+
+passwordResetTokenSchema.methods.compareToken = async function (token) {
+  const result = await bcrypt.compare(token, this.token);
+  return result;
+};
+
+passwordResetTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    this.token = await bcrypt.hash(this.token, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model("PasswordResetToken", passwordResetTokenSchema);
