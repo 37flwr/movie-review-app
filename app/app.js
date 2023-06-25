ef25b6a6f495be46312af5161ea6133a04fe05bb
37flwr <nesterishinyarik@gmail.com>
2023-06-25 21:08:43 +0300

    + error handler

--- app/app.js
+++ app/app.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 require("./db");
 
 const userRouter = require("./routes/user");
+const { errorHandler } = require("./middlewares/error");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/user", userRouter);
+
+app.use(errorHandler);
 
 app.listen(8080, () => {
   console.log("App is running on port http://localhost:8080");
