15aeccb42717661ad61690630b0ef7291c13f6a0
37flwr <nesterishinyarik@gmail.com>
2023-06-27 00:02:54 +0300

    + content to sign in form

--- www/src/components/auth/SignIn/formFields.tsx
+++ www/src/components/auth/SignIn/formFields.tsx
@@ -7,6 +7,12 @@ const formFields = [
     label: "Email",
     type: "text",
   },
+  {
+    name: "password",
+    placeholder: "*********",
+    label: "Password",
+    type: "password",
+  },
 ];
 
 const renderFormFields = () => formFields.map((el) => <FormInput {...el} />);
--- www/src/components/auth/SignIn/index.tsx
+++ www/src/components/auth/SignIn/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import BasicLayout from "../../layout/BasicLayout";
 import Title from "../../form/Title";
 import renderFormFields from "./formFields";
+import Submit from "../../form/Submit";
 
 const SignIn = () => {
   return (
@@ -10,6 +11,22 @@ const SignIn = () => {
         <form action="" className="bg-secondary rounded p-6 w-96 space-y-5">
           <Title>Sign in</Title>
           {renderFormFields()}
+          <Submit value="Sign in" />
+
+          <div className="flex justify-between">
+            <a
+              className="text-dark-subtle hover:text-white transition"
+              href="#"
+            >
+              Forget Password
+            </a>
+            <a
+              className="text-dark-subtle hover:text-white transition"
+              href="#"
+            >
+              Sign up
+            </a>
+          </div>
         </form>
       </BasicLayout>
     </div>
