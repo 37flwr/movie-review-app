1456f99b78999c03dfcee705a5b5fc836cf8f963
37flwr <nesterishinyarik@gmail.com>
2023-06-26 23:47:26 +0300

    + form input component

--- www/src/components/auth/SignIn.tsx
+++ www/src/components/auth/SignIn.tsx
@@ -1,27 +1,15 @@
 import React from "react";
 import BasicLayout from "../layout/BasicLayout";
 import Title from "../form/Title";
+import FormInput from "../form/FormInput";
 
 const SignIn = () => {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
       <BasicLayout>
-        <form action="" className="bg-secondary rounded p-6 w-96">
+        <form action="" className="bg-secondary rounded p-6 w-96 space-y-5">
           <Title>Sign in</Title>
-          <div className="flex flex-col-reverse">
-            <input
-              type="text"
-              id="email"
-              className="bg-transparent rounded border-2 border-dark-subtle w-full text-lg outline-none focus:border-white p-1 text-white peer transition"
-              placeholder="john@email.com"
-            />
-            <label
-              htmlFor="email"
-              className="font-semibold text-dark-subtle peer-focus:text-white transition self-start"
-            >
-              Email
-            </label>
-          </div>
+          <FormInput name="email" label="Email" placeholder="john@email.com" />
         </form>
       </BasicLayout>
     </div>
