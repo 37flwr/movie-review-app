9fdfd44a86c91d191a04cb00bbd611feda1ccda0
37flwr <nesterishinyarik@gmail.com>
2023-06-26 23:31:04 +0300

    + basic sign in form

--- www/src/components/auth/SignIn.tsx
+++ www/src/components/auth/SignIn.tsx
@@ -9,15 +9,16 @@ const SignIn = () => {
           <h1 className="text-xl text-white font-semibold text-center">
             Sign in
           </h1>
-          <div>
+          <div className="flex flex-col-reverse">
             <input
               type="text"
+              id="email"
               className="bg-transparent rounded border-2 border-dark-subtle w-full text-lg outline-none focus:border-white p-1 text-white peer transition"
               placeholder="john@email.com"
             />
             <label
               htmlFor="email"
-              className="font-semibold text-dark-subtle peer-focus:text-white transition"
+              className="font-semibold text-dark-subtle peer-focus:text-white transition self-start"
             >
               Email
             </label>
